a95355085d5ea6dbec308f0a994e87dc13e1b597
Brandt Damman <brandtdamman@gmail.com>
2020-08-05 21:18:04 -0500

    QR scanner now works with all devices via HTTPS.

--- src/main.py
+++ src/main.py
@@ -4,4 +4,5 @@ from Timecard import app
 
 if __name__ == '__main__':
     #app.run()
-    app.run(host=str(sys.argv[1]))
+    #app.run(host=str(sys.argv[1]))
+    app.run(host=str(sys.argv[1]), ssl_context='adhoc')
