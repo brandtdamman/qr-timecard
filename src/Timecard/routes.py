010b550b233b5c2bf98ff045aa13b5ec57946e87
Brandt Damman <brandtdamman@gmail.com>
2020-08-06 22:17:20 -0500

    Working QR scan with username.

--- src/Timecard/routes.py
+++ src/Timecard/routes.py
@@ -1,8 +1,37 @@
-from flask import render_template
+from flask import render_template, url_for, redirect, request
 from Timecard import app
+
+from datetime import datetime
+from time import localtime
 
 # Routes
 @app.route('/')
 def index():
     """ Returns the homepage. """
     return render_template('index.html')
+
+@app.route('/readcode.html', methods=['GET', 'POST'])
+def readcode():
+    """ QR Code scanner page. """
+    if request.method == 'POST':
+        import os
+        import json
+
+        if not os.path.exists("./timecard"):
+            open("./timecard", "w+").close()
+        
+        clock = open("./timecard", "a")
+
+        # clock.write(f"{request.data}")
+        data = json.loads(request.data.decode("utf-8"))
+
+        time = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+        if data.get('type') == 'In':
+            clock.write(f"[{time}]: User '{data.get('user')}' clocked in at {data.get('location')}\n")
+        else:
+            clock.write(f"[{time}]: User '{data.get('user')}' clocked out at {data.get('location')}\n")
+
+        clock.close()
+        return render_template('index.html')
+
+    return render_template('readcode.html')
--- src/main.py
+++ src/main.py
@@ -5,4 +5,4 @@ from Timecard import app
 if __name__ == '__main__':
     #app.run()
     #app.run(host=str(sys.argv[1]))
-    app.run(host=str(sys.argv[1]), ssl_context='adhoc')
+    app.run(host=str(sys.argv[1]), ssl_context=('cert.pem', 'key.pem'))
