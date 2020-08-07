939db35f821cc18b7bdf490ea18391ae84b94080
Brandt Damman <brandtdamman@gmail.com>
2020-08-07 15:56:23 -0500

    Added comments, appended to README, and cleaned up code a touch.

--- src/main.py
+++ src/main.py
@@ -3,6 +3,6 @@ import sys
 from Timecard import app
 
 if __name__ == '__main__':
-    #app.run()
-    #app.run(host=str(sys.argv[1]))
+    # TODO: Update SSL information for Gunicorn and Nginx outside of general implementation.
+    # You will need a self-signed cert.pem or key.pem file.
     app.run(host=str(sys.argv[1]), ssl_context=('cert.pem', 'key.pem'))
